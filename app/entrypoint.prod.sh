11d8aff52a8a7caa00360471deed7204e9f12b54
alekorn <entkorn@gmail.com>
2024-06-07 15:03:11 +0300

    add waiting for postgres db

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 set -ex
 
+wait_for() {
+    timeout=60
+    while ! nc -z "$1" "$2" >/dev/null 2>&1; do
+        echo "Waiting for $1:$2..."
+        sleep 1
+        timeout=$((timeout - 1))
+        if [ $timeout -eq 0 ]; then
+            echo "Timeout waiting for $1:$2"
+            exit 1
+        fi
+    done
+}
+
+wait_for db 5432
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 
